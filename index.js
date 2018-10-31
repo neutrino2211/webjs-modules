e1d4ae895ed5994c93c6c0db0523769e757fd2bb
neutrino2211 <neutrino2211@gmail.com>
2018-10-31 22:43:53 +0100

    onsenui cleanup

--- index.js
+++ index.js
@@ -1,30 +1,32 @@
-import "onsenui/css/onsenui.css";
-import "onsenui/css/onsen-css-components.css"
-import "onsenui/css/ionicons/css/ionicons.css"
+import "./onsenui/css/onsenui.css";
+import "./onsenui/css/onsen-css-components.css"
+import "./onsenui/css/ionicons/css/ionicons.css"
 
-import ons from "onsenui";
-export function init(){
-    var os = ""
-    var userAgent = navigator.userAgent || navigator.vendor || window.opera;
-
-      // Windows Phone must come first because its UA also contains "Android"
-    if (/windows phone/i.test(userAgent)) {
-        os = "wp";
+import _ons from "./onsenui/js/onsenui";
+export const ons = _ons;
+export var UI = {
+    init(){
+        var os = ""
+        var userAgent = navigator.userAgent || navigator.vendor || window.opera;
+    
+          // Windows Phone must come first because its UA also contains "Android"
+        if (/windows phone/i.test(userAgent)) {
+            os = "wp";
+        }
+    
+        else if (/android/i.test(userAgent)) {
+            os = "android";
+        }
+    
+        // iOS detection from: http://stackoverflow.com/a/9039885/177710
+        else if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
+            os = "ios";
+        }
+    
+        else {
+            os = "android"
+        }
+        console.log("OS:",os)
+        _ons.platform.select(os);
     }
-
-    else if (/android/i.test(userAgent)) {
-        os = "android";
-    }
-
-    // iOS detection from: http://stackoverflow.com/a/9039885/177710
-    else if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
-        os = "ios";
-    }
-
-    else {
-        os = "wp"
-    }
-    console.log("OS:",os)
-    ons.platform.select(os)
-    window.ons = ons;
 }
